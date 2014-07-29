be8aef51dca3c4b3692c561b816016c8f1a423c8
icaker <puzzlezzz@gmail.com>
2014-07-29 16:38:18 +0800

    forget what has done

--- solve_distribute/solve.c
+++ solve_distribute/solve.c
@@ -16,20 +16,32 @@ int main(int argc,char *argv[]){
 
   glp_add_cols(mip, 3);
   glp_set_col_name(mip, 1, "x1");
-  glp_set_col_bnds(mip, 1, GLP_DB, 0, n[0]);
-  glp_set_obj_coef(mip, 1, q[0]);   //queue length
+  glp_set_col_bnds(mip, 1, GLP_DB, 0.0, n[0]);
+  glp_set_obj_coef(mip, 1, q[0]);
+  glp_set_col_kind(mip, 1, GLP_IV);
+
   glp_set_col_name(mip, 2, "x2");
-  glp_set_col_bnds(mip, 2, GLP_DB, 0, n[1]);
+  //if (n[1]==0)
+  //  glp_set_col_bnds(mip, 2, GLP_FX, 0.0, n[1]);
+  //else
+  glp_set_col_bnds(mip, 2, GLP_DB, 0.0, n[1]);
   glp_set_obj_coef(mip, 2, q[1]);
+  glp_set_col_kind(mip, 2, GLP_IV);
+
   glp_set_col_name(mip, 3, "x3");
-  glp_set_col_bnds(mip, 3, GLP_DB, 0, n[2]);
+  //if (n[2]==0)
+  //  glp_set_col_bnds(mip, 3, GLP_FX, 0.0, n[2]);
+  //else
+  glp_set_col_bnds(mip, 3, GLP_DB, 0.0, n[2]);
   glp_set_obj_coef(mip, 3, q[2]);
+  glp_set_col_kind(mip, 3, GLP_IV);
+
 
   int ia[1+3], ja[1+3];
   double ar[1+3];
-  ia[1]=1,ja[1]=1,ar[1]=1;   // a[1,1] = 1
-  ia[2]=1,ja[2]=2,ar[2]=5;    // a[1,2] = 5
-  ia[3]=1,ja[3]=3,ar[3]=10;    // a[1,3] = 10
+  ia[1]=1,ja[1]=1,ar[1]=10;   // a[1,1] = 10
+  ia[2]=1,ja[2]=2,ar[2]=1;    // a[1,2] = 1
+  ia[3]=1,ja[3]=3,ar[3]=0.1;    // a[1,3] = 0.1
 
   glp_load_matrix(mip, 3, ia, ja, ar);
 
@@ -40,12 +52,12 @@ int main(int argc,char *argv[]){
 
 
   //glp_simplex(mip, NULL);
-  double t = glp_mip_obj_val(mip);
+  //double t = glp_mip_obj_val(mip);
   int x = glp_mip_col_val(mip, 1);
   int y = glp_mip_col_val(mip, 2);
   int z = glp_mip_col_val(mip, 3);
 
-  printf("\nt = %g;x = %d; y = %d; z = %d\n", t, x, y, z);
+  printf("%d,%d,%d\n", x, y, z);
 
   glp_delete_prob(mip);
   return 0;
